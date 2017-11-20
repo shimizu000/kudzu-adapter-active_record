cdf74a20cdf3d90a20d71e6a9b50f7b4500236c6
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-11-20 09:36:23 +0900

    Fix requires

--- lib/kudzu/adapter/active_record.rb
+++ lib/kudzu/adapter/active_record.rb
@@ -1,7 +1,13 @@
 require 'activerecord-import'
 require 'kudzu'
-require 'kudzu/adapter/active_record/all'
+require_relative 'active_record/model/base'
+require_relative 'active_record/model/page'
+require_relative 'active_record/model/content'
+require_relative 'active_record/model/link'
+require_relative 'active_record/frontier'
+require_relative 'active_record/repository'
 
 Kudzu.adapter = Kudzu::Adapter::ActiveRecord
 Kudzu::Page = Kudzu::Adapter::ActiveRecord::Page
 Kudzu::Link = Kudzu::Adapter::ActiveRecord::Link
+Kudzu::Content = Kudzu::Adapter::ActiveRecord::Content
