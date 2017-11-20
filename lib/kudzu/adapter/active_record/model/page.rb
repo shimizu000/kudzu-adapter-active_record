a75682ec2692b204ffdc85383b42411d4410e260
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-11-20 10:06:43 +0900

    Fix response_header encoding

--- lib/kudzu/adapter/active_record/model/page.rb
+++ lib/kudzu/adapter/active_record/model/page.rb
@@ -6,7 +6,28 @@ module Kudzu
 
         has_one :content, dependent: :destroy
 
-        serialize :response_header, JSON
+        def response_header
+          if response_header_column_is_text?
+            JSON.parse(super)
+          else
+            super
+          end
+        end
+
+        def response_header=(val)
+          if response_header_column_is_text?
+            super(JSON.generate(val))
+          else
+            super
+          end
+        end
+
+        private
+
+        def response_header_column_is_text?
+          type = self.class.columns_hash["response_header"].type
+          type == :text || type == :string
+        end
       end
     end
   end
