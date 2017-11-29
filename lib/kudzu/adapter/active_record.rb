ff614158e994d3ea61b2fc64c6e0824ef4518452
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-11-29 16:13:50 +0900

    Fix require of ActiveRecord models

--- lib/kudzu/adapter/active_record.rb
+++ lib/kudzu/adapter/active_record.rb
@@ -1,13 +1,12 @@
 require 'activerecord-import'
 require 'kudzu'
-require_relative 'active_record/model/base'
-require_relative 'active_record/model/page'
-require_relative 'active_record/model/content'
-require_relative 'active_record/model/link'
-require_relative 'active_record/frontier'
-require_relative 'active_record/repository'
 
 Kudzu.adapter = Kudzu::Adapter::ActiveRecord
-Kudzu::Page = Kudzu::Adapter::ActiveRecord::Page
-Kudzu::Link = Kudzu::Adapter::ActiveRecord::Link
-Kudzu::Content = Kudzu::Adapter::ActiveRecord::Content
+
+if defined? Railtie
+  ActiveSupport.on_load :active_record do
+    require_relative 'active_record/all'
+  end
+else
+  require_relative 'active_record/all'
+end
