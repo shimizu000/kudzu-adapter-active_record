173b730c87cb515f73e84ffab4b02941d67a0ee0
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2017-12-20 11:22:38 +0900

    Remove save_content option

--- lib/kudzu/adapter/active_record/repository.rb
+++ lib/kudzu/adapter/active_record/repository.rb
@@ -2,16 +2,12 @@ module Kudzu
   module Adapter
     module ActiveRecord
       class Repository
-        def initialize(config)
-          @config = config
-        end
-
         def find_by_url(url)
           Page.where(url: url).first_or_initialize
         end
 
         def register(page)
-          if @config.save_content
+          if page.body
             content = page.content || page.build_content
             content.data = page.body
           end
